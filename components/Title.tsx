6d0b4cfff7b30d8e2a11aa308aa799093f604bb3
CodeWithMilap <milapdave15@gmail.com>
2024-01-21 15:36:23 +0530

    Update Title.tsx

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,22 +1,28 @@
 // components/Title.tsx
 
-import React, { FC } from 'react';
+import React, { FC } from "react";
 
 interface TitleProps {
   title: string;
-  content: string;
+  content?: string;
   className?: string;
+  fontSize?: string;
 }
 
-const Title: FC<TitleProps> = ({ title, content, className }) => {
+const Title: FC<TitleProps> = ({
+  title,
+  content,
+  className = '',
+  fontSize = "lg:text-5xl text-3xl",
+}) => {
   return (
-    <div className={`mx-auto  flex flex-col gap-6 ${className}`}>
-      <h2 className="lg:text-5xl text-3xl font-extrabold leading-tight">
-        {title}
-      </h2>
-      <p className="text-xl font-opensans text-Grey leading-snug">
-        {content}
-      </p>
+    <div className={`flex flex-col gap-6 ${className}`}>
+      <h2 className={`font-extrabold leading-tight ${fontSize}`}>{title}</h2>
+      {content && (
+        <p className="font-opensans text-Grey leading-snug">
+          {content}
+        </p>
+      )}
     </div>
   );
 };
