9c63a1c7471d00c209d80d8ee5259cbf360d2d94
CodeWithMilap <milapdave15@gmail.com>
2024-01-21 15:36:49 +0530

    Update ClientLogos.tsx

--- components/ClientLogos.tsx
+++ components/ClientLogos.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import React from "react";
+import Section from "./Section";
 
 interface ClientLogosProps {
   logos: string[];
@@ -9,23 +10,24 @@ interface ClientLogosProps {
 
 const ClientLogos: React.FC<ClientLogosProps> = ({ logos }) => {
   return (
-    <section className="client-logos">
-      <div className="max-w-screen-xl mx-auto lg:px-8 px-4">
-        <div className="grid grid-cols-6">
-          {logos.map((logo, index) => (
-            <div className="lg:col-span-1 col-span-2 flex items-center justify-center" key={index}>
-              <Image
-                src={`/logos/${logo}`}
-                className="w-full"
-                alt={`Client Logo`}
-                width={185}
-                height={132}
-              />
-            </div>
-          ))}
-        </div>
+    <Section>
+      <div className="grid grid-cols-6">
+        {logos.map((logo, index) => (
+          <div
+            className="lg:col-span-1 col-span-2 flex items-center justify-center"
+            key={index}
+          >
+            <Image
+              src={`/logos/${logo}`}
+              className="w-full"
+              alt={`Client Logo`}
+              width={185}
+              height={132}
+            />
+          </div>
+        ))}
       </div>
-    </section>
+    </Section>
   );
 };
 
