55bb50c8ee49c254428073cdf0da71576d43fb66
CodeWithMilap <milapdave15@gmail.com>
2024-02-11 12:20:19 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Header from "@/components/Header";
 const manrope = Manrope({
   subsets: ["latin"],
   variable: "--font-manrope",
-  weight: ["400", "500", "600", "700"],
+  weight: ["400", "500", "600", "700", "800"],
 });
 const opensans = Open_Sans({
   subsets: ["latin"],
