1631a01b0d4bcd146783a08b659c9d0e9a86b56f
CodeWithMilap <milapdave15@gmail.com>
2024-04-29 09:24:28 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Section from './Section'
 import Link from 'next/link'
-import Logo from './Logo'
-
+import LogoImg from "../public/logo_blue.svg";
+import Image from "next/image";
 const Footer = () => {
     return (
         <footer className='bg-Background'>
             <Section className=''>
-                <div className='grid grid-cols-12 md:py-16'>
-                    <div className='col-span-5 -mt-8'><Logo /></div>
-                    <div className='col-span-2 flex flex-col gap-5'>
+                <div className='grid grid-cols-1 lg:grid-cols-12 py-10'>
+                    <div className='lg:col-span-5 py-5'><Image className="lg:h-12 h-8 w-auto" src={LogoImg} alt="" /></div>
+                    <div className='lg:col-span-2 flex flex-col gap-5 py-5'>
                         <h6 className='text-Black font-bold text-base'>Quick link</h6>
                         <div className='flex flex-col gap-2'>
                             <Link href={'#'}>Home</Link>
@@ -18,7 +18,7 @@ const Footer = () => {
                             <Link href={'#'}>Solutions</Link>
                         </div>
                     </div>
-                    <div className='col-span-2 flex flex-col gap-5'>
+                    <div className='lg:col-span-2 flex flex-col gap-5 py-5'>
                         <h6 className='text-Black font-bold text-base'>Service</h6>
                         <div className='flex flex-col gap-2'>
                             <Link href={'#'}>Home</Link>
@@ -27,7 +27,7 @@ const Footer = () => {
                             <Link href={'#'}>Solutions</Link>
                         </div>
                     </div>
-                    <div className='col-span-3 flex flex-col gap-5'>
+                    <div className='lg:col-span-3 flex flex-col gap-5 py-5'>
                         <h6 className='text-Black font-bold text-base'>Cantact info</h6>
                         <div className='flex flex-col gap-2'>
                             <Link href={'#'}>ensome@info.co.us</Link>
@@ -36,7 +36,7 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
-                <div className='w-full md:py-10 py-6 border-t border-Helper-blue-3 flex justify-between'>
+                <div className='w-full md:py-10 py-6 border-t border-Helper-blue-3 grid grid-cols-1 lg:grid-cols-2 gap-3 justify-between'>
                     <div className='flex gap-6'>
                         <Link href="#">Privacy policy</Link>
                         <Link href="#">Terms of us</Link>
